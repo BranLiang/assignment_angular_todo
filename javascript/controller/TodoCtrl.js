e94cb42a2f373d274f6e2c4d1f041754ef9e223a
BranLiang <lby89757@hotmail.com>
2016-09-29 11:15:04 +0800

    delete some comments

--- javascript/controller/TodoCtrl.js
+++ javascript/controller/TodoCtrl.js
@@ -13,31 +13,16 @@ TodoApp.controller('TodoCtrl', ['$scope', 'todoService', function ($scope, todoS
 	};
 
 	$scope.createTodo = function () {
-		// var item = {
-		// 	text: $scope.taskParams.text,
-		// 	dueDate: $scope.taskParams.date,
-		// 	complete: false
-		// };
-		// $scope.items.push(item);
-		// console.log($scope.items);
-		// $scope.taskParams = {};
 		todoService.createItem($scope.taskParams)
 		$scope.taskParams = {};
 	};
 
 	$scope.deleteTask = function (item) {
-		// var index = $scope.items.indexOf(item);
-		// $scope.items.splice(index, 1);
 		console.log(item);
 		todoService.deleteItem(item);
 	};
 
 	$scope.clearComplete = function () {
-		// for (var i = 0; i < $scope.items.length; i++) {
-		// 	if ($scope.items[i].complete === true) {
-		// 		$scope.deleteTask($scope.items[i]);
-		// 	};
-		// };
 		todoService.clearComplete();
 	};
 
